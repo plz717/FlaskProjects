081e93852de5258e81038bd680b36a884eb2c066
linzhuo.plz <linzhuo.plz@alibaba-inc.com>
2020-12-04 17:19:27 +0800

    add

--- flask-blog/app.py
+++ flask-blog/app.py
@@ -1,13 +1,66 @@
+import os
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+import click
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# 设置变量以告诉数据库连接地址
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
+db = SQLAlchemy(app) # 初始化扩展，传入程序实例app
+
+
+# 创建数据库模型
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20))
+
+
+class Diary(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    create_time = db.Column(db.DateTime)
+    title = db.Column(db.String(60))
+    content = db.Column(db.Text)
+
+
+# 自定义数据库初始化命令initdb
+@app.cli.command()
+@click.option('--drop', is_flag = True, help = 'create after drop')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo("Initialized database.")
+
+
+# 假数据
+def fake_data():
+    user = User(name = '小胖儿')
+    d1 = Diary(title = 'my first diary', content = 'Im so happy today!', create_time = datetime.datetime.utcnow().strftime("%b %d %Y, %H:%M"))
+    d2 = Diary(title = 'my second diary', content = 'Today is so exciting!', create_time = datetime.datetime.utcnow().strftime("%b %d %Y, %H:%M"))
+    d3 = Diary(title = 'my third diary', content = 'So hard my way...', create_time = datetime.datetime.utcnow().strftime("%b %d %Y, %H:%M"))
+    diaries = [d1, d2, d3]
+
+    print("d1.create_time:", d1.create_time)
+    print("d2.create_time:", d2.create_time)
+
+    return diaries
+
+
+
 
 # 首页
 @app.route('/')
 def hello():
     # return 'Welcome to My World!'
     # return '<h1>Hello my Friend！</h1><img src="http://helloflask.com/totoro.gif">'
-    return render_template('homepage.html', username='小胖儿')
+    diaries = fake_data()
+    return render_template('homepage.html', username='小胖儿', diaries = diaries)
+    # return render_template('homepage.html', username = '小胖儿')
 
 # template: 包含变量和运算逻辑的html/其他格式文本, 默认存储在项目根目录中（即与app.py相同的目录下）
 # 渲染：进行变量替换及逻辑运算的过程，jinja2就是一个模版渲染引擎
