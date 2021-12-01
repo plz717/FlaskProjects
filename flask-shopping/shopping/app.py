b59d90c50fb00877592632657209c93fd327cccf
plz717 <panglinzhuo@gmail.com>
2021-12-01 12:47:02 +0800

    update homepage

--- flask-shopping/shopping/app.py
+++ flask-shopping/shopping/app.py
@@ -5,7 +5,7 @@ from flask import render_template, request, flash
 
 def add_user_to_database(username, password):
     '''
-    return 0: 注册成功，-1: 此用户已注册，-2: 此用户已注册但密码输入错误
+    return 0: 注册成功，-1: 用户已存在
     '''
     res = Buyer.query.filter_by(name=username).first()
     if res is not None: # 此用户已注册
@@ -15,6 +15,19 @@ def add_user_to_database(username, password):
         db.session.add(user)
         db.session.commit()
         return 0
+    
+def check_user_login(username, password):
+    '''
+    return 0: 登陆成功，-1：用户不存，-2: 密码错误
+    '''
+    res = Buyer.query.filter_by(name=username).first()
+    if res is None: # 用户不存在
+        return -1
+    if res.password != password: # 密码错误
+        return -2
+    return 0
+
+
 
 
 @app.route('/hello')
@@ -45,5 +58,19 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        
-    return render_template('login.html')
+        username = request.form.get("username")
+        password = request.form.get("password")
+        ret = check_user_login(username, password)
+        goods = Good.query.all()
+        if ret == 0:
+            flash("登陆成功!")
+            return render_template("homepage.html", goods = goods, username = username)
+        if ret == -1:
+            flash("用户不存在，请注册！")
+            return render_template("homepage.html", goods = goods, username = None)
+        if ret == -2:
+            flash("密码错误, 请重新输入！")
+            return render_template('login.html')
+
+    else:
+        return render_template('login.html')
